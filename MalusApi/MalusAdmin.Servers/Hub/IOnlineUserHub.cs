ae170b8265e912800f2ef7dbba0bc9eaac878285
pridejoy <pridejoy@foxmail.com>
2024-05-30 15:58:44 +0800

    在线用户

---
+++ MalusApi/MalusAdmin.Servers/Hub/IOnlineUserHub.cs
@@ -0,0 +1,33 @@
+﻿namespace MalusAdmin.Servers.Hub;
+
+public interface IOnlineUserHub
+{
+    /// <summary>
+    ///     在线用户列表
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    Task OnlineUserList(object context);
+
+    /// <summary>
+    ///     强制下线
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    Task ForceOffline(object context);
+
+
+    /// <summary>
+    ///     发布上线通知消息
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    Task PublicNotice(string context);
+
+    /// <summary>
+    ///     接收消息
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns></returns>
+    Task ReceiveMessage(object context);
+}
---
+++ MalusApi/MalusAdmin.Servers/SysOnlineUser/SysOnlineUserService.cs
@@ -0,0 +1,23 @@
+﻿using ICacheService = MalusAdmin.Common.ICacheService;
+
+namespace MalusAdmin.Servers.SysOnlineUser;
+
+/// <summary>
+/// </summary>
+public class SysOnlineUserService
+{
+    private readonly ICacheService _cacheService;
+
+    public SysOnlineUserService(ICacheService cacheService)
+    {
+        _cacheService = cacheService;
+    }
+
+    // public async Task<PageList<dynamic>> OnlineUserPage()
+    // {
+    //    var userkey= await _cacheService.GetKeysByPatternAsync(Constant.Cache.OnlineUser);
+    //    var list = _cacheService.get<OnlineUser>();
+    //
+    //     return "";
+    // }
+}
